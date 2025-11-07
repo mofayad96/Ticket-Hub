14dcd058e9b1ec08d05270b766ad469c6678da3d
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2025-11-07 13:32:30 +0200

    fixing errors

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env?.VITE_API_URL || '/1api',
+  baseURL: import.meta.env?.VITE_API_URL || '/api',
   withCredentials: true
 })
 
