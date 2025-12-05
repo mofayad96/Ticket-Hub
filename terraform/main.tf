ba0bc857eda14c7dbabbe08319a48b356c1663be
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2025-12-05 15:02:10 +0200

    fixing errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -158,6 +158,27 @@ resource "aws_security_group" "app_sg" {
     ManagedBy   = "terraform"
   }
 }
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = { Service = "ec2.amazonaws.com" },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
 
 # EC2 Instance for Application with password-based SSH
 resource "aws_instance" "app_server" {
